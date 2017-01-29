12fe5069ffa89d5883ba9cfe1f43cb524290efc6
MasahiroNakajima <masahiro.nakajima.1202@gmail.com>
2017-01-29 23:55:11 +0900

    距離による評価クラスのモック作成

---
+++ game/Assets/Scripts/Battle/PointQuerySystem/Scorer/ScorerDistanceToObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Commander.Battle.AI
+{
+    public abstract class ScorerDistanceToObject : Scorer
+    {
+        protected override void PreProcess()
+        {
+        }
+
+        protected override void ApplyToPoint(ScoreingPoint point)
+        {
+        }
+    }
+}
