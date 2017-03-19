3e642028572f68311305e2e71a68bedceba086da
MasahiroNakajima <masahiro.nakajima.1202@gmail.com>
2017-03-19 11:00:16 +0900

    Actor同士の当たり判定実装

--- game/Assets/Scripts/Battle/ActorCollision.cs
+++ game/Assets/Scripts/Battle/ActorCollision.cs
@@ -10,14 +10,38 @@ namespace Commander.Battle
     /// </summary>
     public class ActorCollision : MonoBehaviour
     {
+        static readonly float Epcilon = 0.005f;
+
         [SerializeField]
         Actor owner;
 
         public Actor Owner { get { return owner; } }
 
-        private void OnCollisionEnter(Collision collision)
+        public void OnTriggerStay(Collider other)
         {
+            if (owner == null) { return; }
+            HitCheckToActor(other);
+        }
 
+        void HitCheckToActor(Collider other)
+        {
+            var otherCollision = other.gameObject.GetComponent<ActorCollision>();
+            if (otherCollision == null) { return; }
+            if (otherCollision.Owner == null) { return; }
+
+            var otherActor = otherCollision.Owner;
+
+            Vector3 toOther = otherActor.transform.position - owner.transform.position;
+            toOther.y = 0.0f;
+            toOther.Normalize();
+
+            Vector3 otherPosition = otherActor.transform.position;
+            otherPosition += toOther * Epcilon;
+            otherActor.transform.position = otherPosition;
+
+            Vector3 actorPosition = owner.transform.position;
+            actorPosition -= toOther * Epcilon;
+            owner.transform.position = actorPosition;
         }
     }
 }
